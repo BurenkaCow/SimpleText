66d0508a169db91817b7239339711b379d96770d
Nikitin.I <nikitin.i@vhm.ru>
2017-10-26 10:46:20 +0300

    -+: Полностью переписан алгоритм

--- simpleText.cpp
+++ simpleText.cpp
@@ -1,43 +1,36 @@
 #include "stdafx.h"
-#include <set>
 #include <random>
-
-template<typename T>
-auto FindMax(const std::multiset<T>& set)
-{
-	std::pair<std::multiset<T>::iterator, std::multiset<T>::iterator> pair;
-
-	if (set.begin() != set.end())
-	{
-		auto value = --set.end();
-		pair.first = value;
-		if (value != set.begin())
-		{
-			pair.second = --value;
-		}
-		else
-		{
-			pair.second = set.end();
-		}
-	}
-	else
-	{
-		pair.first = pair.second = set.end();
-	}
-	
-	return pair;
-}
 
 
 int main()
 {
-	std::multiset<int> set;
-	const unsigned int sizeArray = 10;
-	for (unsigned int i = 0; i < sizeArray; i++)
+	int array[10];
+	for (unsigned char i = 0; i < 10; i++)
 	{
-		set.insert(std::rand() % 10);
+		array[i] = rand() % 10;
 	}
-	auto twoMaxNumbers = FindMax(set); // функция находит пару итераторов максимальных значений любого multise
+	int maxValue1 = array[0];
+	int maxValue2 = array[0];
+	int* point = &maxValue1;
+	for (unsigned char i = 1; i < 10; i++)
+	{
+		if (array[i] > *point)
+		{
+			*point = array[i];
+			if (point == &maxValue1)
+			{
+				point = &maxValue2;
+			}
+			else
+			{
+				point = &maxValue1;
+			}
+		}
+	}
+	if (maxValue1 < maxValue2) 
+	{
+		std::swap(maxValue1, maxValue2);
+	}
 
     return 0;
 }
